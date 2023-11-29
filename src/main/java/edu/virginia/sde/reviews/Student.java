7dda7545e92ea80e98e2f8fb72a56aa98731ac0e
bcham <vpd4ve@virginia.edu>
2023-11-29 12:37:48 -0500

    Created Student class

---
+++ src/main/java/edu/virginia/sde/reviews/Student.java
@@ -0,0 +1,11 @@
+package edu.virginia.sde.reviews;
+
+public class Student {
+    private String username;
+    private String password;
+
+    public Student(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+}
