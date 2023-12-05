9dab093f867d827f89429944b9ac879b9a6ba1ee
bcham <vpd4ve@virginia.edu>
2023-12-05 11:57:42 -0500

    Fixed myReviews Selecting to send to review search screen

--- src/main/java/edu/virginia/sde/reviews/MyReviewsScreenController.java
+++ src/main/java/edu/virginia/sde/reviews/MyReviewsScreenController.java
@@ -19,9 +19,13 @@ import static edu.virginia.sde.reviews.LoginScreenController.loggedStudentUserna
 public class MyReviewsScreenController {
 
     @FXML
-    private Button backButton;
+    private Button backButton, viewReviewButton;
 
     public TableView<MyReviewsTable> tableMyReviews;
+
+    private static String mnemonic;
+    private static int number;
+    private static String title;
 
     @FXML
     private void initialize() {
@@ -50,6 +54,7 @@ public class MyReviewsScreenController {
         configureTableColumn("Course Rating", "courseRating", Integer.class);
 
         tableMyReviews.setOnMouseClicked(this::handleRowClick);
+        setReviewItems(false);
     }
 
 
@@ -64,9 +69,29 @@ public class MyReviewsScreenController {
             MyReviewsTable selectedRow = tableMyReviews.getSelectionModel().getSelectedItem();
             if (selectedRow != null) {
                 System.out.println("Clicked on row: " + selectedRow.getCourseTitle());
+                mnemonic = selectedRow.getCourseMnemonic();
+                number = selectedRow.getCourseNumber();
+                title = selectedRow.getCourseTitle();
+                setReviewItems(true);
             }
         }
     }
+    private void setReviewItems(boolean b) {
+        viewReviewButton.setVisible(b);
+    }
+
+    public static String getMnemonic() {
+        return mnemonic;
+    }
+
+    public static int getNumber() {
+        return number;
+    }
+
+    public static String getTitle() {
+        return title;
+    }
+
 
     @FXML
     private void backButtonAction() {
@@ -76,4 +101,12 @@ public class MyReviewsScreenController {
         backButton.getScene().getWindow().hide();
     }
 
+
+    @FXML
+    private void viewReviewButtonAction() {
+        CourseReviewsApplication mainApp = new CourseReviewsApplication();
+        Stage stage = new Stage();
+        mainApp.showCourseReviewsScreen(stage);
+        viewReviewButton.getScene().getWindow().hide();
+    }
 }
